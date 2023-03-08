d8f0a44c41a88c63927fb535cb98510b954ec3d9
xiaolin-zhong <xz3343@nyu.edu>
2023-03-07 21:00:32 -0500

    edited some files

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 
 from .models import Question, Choice
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
