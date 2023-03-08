b131f6de636a610cb92dbb54a6535593626424e3
xiaolin-zhong <xz3343@nyu.edu>
2023-03-07 21:06:34 -0500

    ran black .

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.question_text
